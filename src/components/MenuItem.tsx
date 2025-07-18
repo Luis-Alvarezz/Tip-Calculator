774db9a2b66ffb4dbc76f7cc50a3ed04e942c049
Luis-Alvarezz <luisangelug10@gmail.com>
2025-07-18 00:05:07 -0600

    [MODIFY]: Se añadio parametro al customHook e indico TIPADO para el metodo addItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,11 +2,11 @@ import type { MenuItem } from "../types/types"
 
 type MenuItemProps = {
   item: MenuItem,
-  addItem: () => void // * PROP (addItem) con TYPE de Arrow Funcion, es decir que recibe método 
+  addItem: (item: MenuItem) => void // * PROP (addItem) con TYPE de Arrow Funcion, es decir que recibe método 
 }
 export default function MenuItem({item, addItem}: MenuItemProps) {
   return (
-    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between" onClick={() => addItem()}>
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between" onClick={() => addItem(item)}>
     <p>{item.name}</p>
     <p className="font-black">${item.price}</p>
     </button>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,12 +1,14 @@
 import { useState } from "react"
-import type { OrderItem } from "../types/types"
+import type { MenuItem, OrderItem } from "../types/types"
 
 export default function useOrder() {
   // * STATE 1 - Orden
   const [order, setOrder] = useState<OrderItem[]>([])
 
-  const addItem = () => {
-    console.log('Agregando...');
+  const addItem = (item: MenuItem) => {
+    // console.log('Agregando...');
+    console.log(item);
+
   }
 
   
