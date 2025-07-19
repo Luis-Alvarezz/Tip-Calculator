0c64312f17be11eb413b75a88e8e9132faa88179
Luis-Alvarezz <luisangelug10@gmail.com>
2025-07-18 23:51:56 -0600

    [MODIFY]: Logica para evitar duplicados y aumentar cantidad de productos

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -8,8 +8,18 @@ export default function useOrder() {
   const addItem = (item: MenuItem) => {
     // console.log('Agregando...');
     // console.log(item);
-    const newItem : OrderItem = {...item, quantity: 1} // * CASTEO para asegurarnos de mantener el TYPE 
-    setOrder([...order, newItem])
+    const itemExist = order.find(orderItem => orderItem.id === item.id) // * Retorna si encontro o no elemento pero NO dice que elemento es
+    // * Por como funciona la MUTABILIDAD en React, NO podemos modificarlo directamtente como en Vue.js
+    if (itemExist) {
+      // console.log('Ya existe');
+      // * 1.- Identificar el Elemento
+      const updatedOrder = order.map( orderItem => orderItem.id === item.id ?  {...orderItem, quantity: orderItem.quantity + 1  } : orderItem)
+      // * 2.- ({...orderItem, quantity: orderItem.quantity + 1  }) -> Conocer que elemento es el duplicado para Tomar que cantidad ya hay e irla incrementado siempre manteniendo objetos que NO se muten directamente
+      setOrder(updatedOrder)
+    } else {
+      const newItem : OrderItem = {...item, quantity: 1} // * CASTEO para asegurarnos de mantener el TYPE 
+      setOrder([...order, newItem])
+    }
   }
 
   console.log(order);
