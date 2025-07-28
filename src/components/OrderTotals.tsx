a8afb6145b2d5170358a3ed288d88c330466bf49
Luis-Alvarezz <luisangelug10@gmail.com>
2025-07-28 00:27:46 -0600

    [ADDED]: Hooks para trabajar con subTotal a pagar, propina y cantidad total a pagar

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -3,28 +3,34 @@ import type { OrderItem } from "../types/types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalsProps = {
-  order: OrderItem[]
+  order: OrderItem[],
+  tip: number
 }
 
-export default function OrderTotals({order}: OrderTotalsProps) {
+export default function OrderTotals({order, tip}: OrderTotalsProps) {
   
   const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0 ), [order])
   // * useMemo nos simplifica el que se ejecute ese codigo cuando cambie la referencia y tambien evita tener ${subTotalAmount}
+
+  const tipAmount = useMemo(() => subTotalAmount * tip , [tip, order]) // * Esta var se ejecuta solo cuando ciertas dependencias cambien
+  // * Esta propina se tiene que ejecutar cuando dos cosas cambien: propina (subir o bajar) o cuando cambie el contenido de la orden
+
+  const totalAmount = useMemo(() => subTotalAmount + tipAmount , [tip, order])
   
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y Propinas</h2>
         <p>Subtotal a pagar: {''}
-          <span className="font-bold">{formatCurrency(subTotalAmount)} </span>
+          <span className="font-bold"> {formatCurrency(subTotalAmount)} </span>
         </p>
 
         <p>Propina: {''}
-          <span className="font-bold">$0</span>
+          <span className="font-bold"> {formatCurrency(tipAmount)} </span>
         </p>
 
         <p>Total a pagar: {''}
-          <span className="font-bold">$0</span>
+          <span className="font-bold"> {formatCurrency(totalAmount)} </span>
         </p>
       </div>
 
